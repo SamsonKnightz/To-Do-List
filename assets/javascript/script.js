3b0b76e333a92787d3d34565680f4b11b202eca2
unknown <samson.vang@live.com>
2024-07-02 18:45:53 -0400

    Started javascript with prompt for creating a new todo

---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+let question = prompt("what would you like to do?");
+let list = ""
+
+if ( question === "New") {
+    prompt("Enter new ToDo")
+    console.log(question)
+    // let newTask = ""
+    // console.log(newTask)
+}
+
