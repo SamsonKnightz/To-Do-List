40b12d16674e7bdbc986f39f775d961bb4b8301d
unknown <samson.vang@live.com>
2024-07-11 20:10:17 -0400

    added other abilities, will not return

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,20 @@
 let question = prompt("what would you like to do?");
-let list = [""]
+let list = [""];
 
-if ( question === "New") {
-    let newTask = prompt("Enter new ToDo")
-    list.push(newTask)
-    console.log(`You added "${newTask}" to the ToDo list`)
+if (question === "New") {
+  let newTask = prompt("Enter new ToDo");
+  list.push(newTask);
+    console.log(`You added "${newTask}" to the ToDo list`);
+} else if (question === "List") {
+  for (let i = 0; i < list.length; i++) {
+    console.log(`${i}: ${list[i]}`);
+  }
+} else if (question === "Delete") {
+  const index = prompt(`Ok, enter an index to delete`);
+  const deleted = list.splice(index, 1);
+    console.log(`Ok, you deleted ${deleted[0]}`);
+} else if (question === " Quit") {
+    console.log("Ok quit the app!");
 }
-else if (question === "List") {
-    for (let i = 0; i < list.length; i++) {
-        console.log(`${i}: ${list[i]}`);
-}
-}
-// else if ( question === "Delete") {
+return;
 
-// }
-// else if ( question === " Quit") {
-
-// }
